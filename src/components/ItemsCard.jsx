4244723497627f8729906824a481fa9642a75fe0
manish2588 <manishkc258@gmail.com>
2025-02-23 09:04:45 +0545

    Added

--- src/components/ItemsCard.jsx
+++ src/components/ItemsCard.jsx
@@ -1,5 +1,6 @@
 import { motion, useAnimation, easeInOut } from "framer-motion";
-import { assets } from "../assets/assets";
+
+import { GiShoppingCart } from "react-icons/gi";
 function ItemsCard({ image, name, price }) {
   const controls = useAnimation();
 
@@ -21,14 +22,14 @@ function ItemsCard({ image, name, price }) {
 
         {/* Hover Effect */}
         <motion.div
-          className="rounded-lg absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-gray-200 opacity-50 grayscale-50"
+          className="rounded-lg absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center z-10 bg-gray-200/50"
           initial={{ scaleY: 0, opacity: 0 }}
           animate={controls}
           transition={{ duration: 0.5, ease: easeInOut }}
           style={{ transformOrigin: "bottom" }}
         >
-          <img src={assets.cart_icon} className="h-6 w-6 " />
-          <h1 className="text-gray-800 font-semibold text-base mt-2">
+          <GiShoppingCart size={35} />
+          <h1 className="text-gray-900  font-serif font-semibold text-lg mt-2">
             ADD TO CART
           </h1>
         </motion.div>
